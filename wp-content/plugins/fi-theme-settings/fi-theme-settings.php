9978ad61b4e1e294cc4af3a0f4f448eb37edba10
Andy <cookies.post@gmail.com>
2015-10-09 08:36:39 +0100

    Add settings page

--- wp-content/plugins/fi-theme-settings/fi-theme-settings.php
+++ wp-content/plugins/fi-theme-settings/fi-theme-settings.php
@@ -1,85 +1,254 @@
 <?php
 /**
- * Plugin Name:       FI Theme Settings
- * Plugin URI:        https://cookie.co.uk
+ * Plugin Name:       First Internet Theme Settings
+ * Plugin URI:        https://firstinternet.co.uk
  * Description:       Add custom settings to a Wordpress theme
  * Version:           0.4.1
- * Author:            Cookie
- * Author URI:        http://cookie.co.uk
+ * Author:            First Internet
+ * Author URI:        https://firstinternet.co.uk
  * Text Domain:       theme-settings
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:       /ac
  * GitHub Plugin URI: https://github.com/fi/theme-settings
  */
+
 /**
- * https://wordpress.org/support/topic/manage_options-role-not-working-as-expected?replies=1#post-7514618
+ * http://www.sitepoint.com/wordpress-options-panel/
  */
 
-class FI_Theme_Settings {
 
-    function __construct() {
-        add_action('admin_menu', array(&$this, 'fi_create_menu'));
-        add_action('admin_init', array(&$this, 'fi_admin_init'));
-       
-    }
-    
-    public function fi_create_menu() {
-        //add_menu_page($page_title, $menu_title, $capability, $menu_slug);
-        add_menu_page('FI Theme Settings', 'FI Theme Settings', 'manage_options', 'fi-theme-settings', array($this, 'fi_file_path'), '
-            dashicons-awards', 10);
-        
-    }
-    
-    public function fi_file_path() {
-       /* if( !current_user_can('manage_opitons') ){
-            wp_die(__('You do not have sufficient permissions to access this page.'));
-        }*/
-        
-        $screen = get_current_screen();
-        if ( strpos( $screen->base, 'fi-theme-settings' ) !== false ) {
-                include( dirname(__FILE__) . '/includes/fi-options.php' );
-        }
-        else {
-                include( dirname(__FILE__) . '/includes/fi-options.php' );
-        }
-    }
-    
-    
-    
-   function fi_admin_init()
-   {
-        register_setting( 'theme_options', 'theme_options', array($this, 'theme_options_validate') );
-        add_settings_section('plugin_main', 'Main Settings', array($this, 'plugin_section_text'), 'plugin');
-        add_settings_field('plugin_text_string', 'Company Name:', array($this, 'fi_company_name_string'), 'plugin', 'plugin_main');
-        
-   }
-   
-   
-   function theme_options_validate($input) {
-       $options = get_option('theme_options');
-        $options['company_name'] = trim($input['company_name']);
-        if(!preg_match('/^[a-z0-9]{32}$/i', $options['company_name'])) {
-            $options['company_name'] = '';
-        }
-        return $options;
-   }
-   
-   
-   function plugin_section_text() {
-       echo "<p>Company Information</p>";
-   }
-   /**
-    * 
-    */
-   function fi_company_name_string() {
-      $options = get_option('theme_options');
-      echo "<input id='fi_setting_string' name='theme_options[company_name]'  size='40' type='text' value='{$options['company_name']}'>";
-   }
-    
+class FI_theme_settings
+{
+    /**
+     * Holds the values to be used in the fields callbacks
+     */
+    private $options;
 
+    /**
+     * Start up
+     */
+    public function __construct()
+    {
+        add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
+        add_action( 'admin_init', array( $this, 'page_init' ) );
+    }
+
+    /**
+     * Add options page
+     */
+    public function add_plugin_page()
+    {
+        // This page will be under "Settings"
+        add_options_page(
+            'Settings Admin', 
+            'FI Theme Settings', 
+            'manage_options', 
+            'fi-setting-admin', 
+            array( $this, 'create_admin_page' )
+        );
+    }
+
+    /**
+     * Options page callback
+     */
+    public function create_admin_page()
+    {
+        // Set class property
+        $this->options = get_option( 'fi_theme_options' );
+        ?>
+        <div class="wrap">
+            <h2>First Internet Theme Settings</h2>           
+            <form method="post" action="options.php">
+            <?php
+                // This prints out all hidden setting fields
+                settings_fields( 'fi_theme_option_group' );   
+                do_settings_sections( 'fi-setting-admin' );
+                submit_button(); 
+            ?>
+            </form>
+        </div>
+        <?php
+    }
+
+    /**
+     * Register and add settings
+     */
+    public function page_init()
+    {        
+        register_setting(
+            'fi_theme_option_group', // Option group
+            'fi_theme_options', // Option name
+            array( $this, 'sanitize' ) // Sanitize
+        );
+
+        add_settings_section(
+            'setting_section_id', // ID
+            'My Custom Settings', // Title
+            array( $this, 'print_section_info' ), // Callback
+            'fi-setting-admin' // Page
+        );  
+
+        add_settings_field(
+            'id_number', // ID
+            'ID Number', // Title 
+            array( $this, 'id_number_callback' ), // Callback
+            'fi-setting-admin', // Page
+            'setting_section_id' // Section           
+        );      
+
+        add_settings_field(
+            'title', 
+            'Title', 
+            array( $this, 'title_callback' ), 
+            'fi-setting-admin', 
+            'setting_section_id'
+        );  
+        
+        add_settings_field(
+            'company_name', 
+            'Company Name', 
+            array( $this, 'company_name_callback' ), 
+            'fi-setting-admin', 
+            'setting_section_id'
+        );      
+        add_settings_field(
+            'company_phone', 
+            'Company Phone', 
+            array( $this, 'company_phone_callback' ), 
+            'fi-setting-admin', 
+            'setting_section_id'
+        );      
+        add_settings_field(
+            'company_lat', 
+            'Company Latitude', 
+            array( $this, 'company_latitude_callback' ), 
+            'fi-setting-admin', 
+            'setting_section_id'
+        );      
+        add_settings_field(
+            'company_long', 
+            'Company Longitude', 
+            array( $this, 'company_longitude_callback' ), 
+            'fi-setting-admin', 
+            'setting_section_id'
+        );      
+        add_settings_field(
+            'has_sidebar', 
+            'Template Has Sidebar', 
+            array( $this, 'template_sidebar_callback' ), 
+            'fi-setting-admin', 
+            'setting_section_id'
+        );      
+    }
+
+    /**
+     * Sanitize each setting field as needed
+     *
+     * @param array $input Contains all settings fields as array keys
+     */
+    public function sanitize( $input )
+    {
+        $new_input = array();
+        if( isset( $input['id_number'] ) )
+            $new_input['id_number'] = absint( $input['id_number'] );
+
+        if( isset( $input['title'] ) )
+            $new_input['title'] = sanitize_text_field( $input['title'] );
+        
+        if( isset( $input['company_name'] ) )
+            $new_input['company_name'] = sanitize_text_field( $input['company_name'] );
+        
+        if( isset( $input['company_phone'] ) )
+            $new_input['company_phone'] = sanitize_text_field( $input['company_phone'] );
+        
+        if( isset( $input['company_lat'] ) )
+            $new_input['company_lat'] = sanitize_text_field( $input['company_lat'] );
+        
+        if( isset( $input['company_long'] ) )
+            $new_input['company_long'] = sanitize_text_field( $input['company_long'] );
+        
+        if( isset( $input['has_sidebar'] ) )
+            $new_input['has_sidebar'] = sanitize_text_field( $input['has_sidebar'] );
+
+        return $new_input;
+    }
+
+    /** 
+     * Print the Section text
+     */
+    public function print_section_info()
+    {
+        print 'Enter your settings below:';
+    }
+
+    /** 
+     * Get the settings option array and print one of its values
+     */
+    public function id_number_callback()
+    {
+        printf(
+            '<input type="text" id="id_number" name="fi_theme_options[id_number]" value="%s" />',
+            isset( $this->options['id_number'] ) ? esc_attr( $this->options['id_number']) : ''
+        );
+    }
+
+    /** 
+     * Get the settings option array and print one of its values
+     */
+    public function title_callback()
+    {
+        printf(
+            '<input type="text" id="title" name="fi_theme_options[title]" value="%s" />',
+            isset( $this->options['title'] ) ? esc_attr( $this->options['title']) : ''
+        );
+    }
+    
+    public function company_name_callback()
+    {
+        printf(
+            '<input type="text" id="company_name" name="fi_theme_options[company_name]" value="%s" />',
+            isset( $this->options['company_name'] ) ? esc_attr( $this->options['company_name']) : ''
+        );
+    }
+    
+    public function company_phone_callback()
+    {
+        printf(
+            '<input type="text" id="company_phone" name="fi_theme_options[company_phone]" value="%s" />',
+            isset( $this->options['company_phone'] ) ? esc_attr( $this->options['company_phone']) : ''
+        );
+    }
+    
+    public function company_latitude_callback()
+    {
+        printf(
+            '<input type="text" id="company_lat" name="fi_theme_options[company_lat]" value="%s" />',
+            isset( $this->options['company_lat'] ) ? esc_attr( $this->options['company_lat']) : ''
+        );
+    }
+    public function company_longitude_callback()
+    {
+        printf(
+            '<input type="text" id="company_long" name="fi_theme_options[company_long]" value="%s" />',
+            isset( $this->options['company_long'] ) ? esc_attr( $this->options['company_long']) : ''
+        );
+    }
+    
+ 
+    
+    public function template_sidebar_callback()
+    {
+       // $value = isset( $this->options['has_sidebar'] ) ? esc_attr( $this->options['has_sidebar']) : '';
+        print '<input type="radio" id="has-sidebar_y" name="fi_theme_options[has_sidebar]" value="1" '.checked( $this->options['has_sidebar'], 1, false ).' /> Yes';
+        print '<input type="radio" id="has-sidebar_n" name="fi_theme_options[has_sidebar]" value="0" '. checked( $this->options['has_sidebar'], 0, false ).' /> No';
+    }
+    
+    
+    
+    
 }
 
-new FI_Theme_Settings();
-
+if( is_admin() )
+    $my_settings_page = new FI_theme_settings();
 
